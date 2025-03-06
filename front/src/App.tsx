ab2a3fa8ebd3d2b5503868539801e57a88592ca2
postwo <codo7717@naver.com>
2025-03-06 20:20:49 +0900

    App.tsx import 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import InputBox from './InputBox'; // InputBox 컴포넌트를 import합니다.
+import InputBox from 'components/inputBox';// InputBox 컴포넌트를 import합니다.
 
 // <InputBox /> 이렇게 return 에 넣어주면 export 한거를 여기서 받아와서 사용한다
 function App() {
