d8db2e656c420755d790b87d43e7bec5002f0b5f
postwo <codo7717@naver.com>
2025-03-12 18:40:42 +0900

    Inputbox 제작 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
 import InputBox from 'components/inputBox';// InputBox 컴포넌트를 import합니다.
 
 // <InputBox /> 이렇게 return 에 넣어주면 export 한거를 여기서 받아와서 사용한다
 function App() {
+ 
+  const [id,setId] =useState<string>('');
+
+  const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) =>{
+    const {value} = event.target;
+    setId(value);
+  }
+
+  const onIdButtonClickHandler= () => {
+
+  }
+ 
+  //이렇게 해서 inputbox만 생성 해서 테스트 
   return (
     <>
-        <InputBox />
+        <InputBox title='아이디' placeholder='아이디를 입력해주세요'type='text' value={id} onChange={onIdChangeHandler} buttonTitle='중복확인' onButtonClick={onIdButtonClickHandler} message='사용 가능한 아이디 입니다' isErrorMessage={false}/>
     </>
   );
 }
--- front/src/components/inputBox/index.tsx
+++ front/src/components/inputBox/index.tsx
@@ -1,12 +1,22 @@
-import {forwardRef} from 'react';
+import {ChangeEvent, forwardRef, KeyboardEvent} from 'react';
 
 // ./는 현재 파일이 위치한 디렉토리(폴더)를 가리킵니다. 따라서 ./style.css는 현재 파일과 같은 디렉토리에 있는 style.css 파일을 가져오는 것을 의미
 // ./: 현재 파일이 위치한 디렉토리
 import './style.css'; 
 
 // 여기다가 타입지정
-interface Props{
-
+interface Props{ // 외부에서 받아오는것들을 여기다가 정리 , //?는 와도 되고 안와도 된다는 뜻이다
+    
+    title: string; // 제목 속성, 문자열 형태로 받아옵니다.
+    placeholder: string; // placeholder 텍스트 속성, 문자열 형태로 받아옵니다.
+    type: 'text' | 'password'; // input 필드의 타입 속성, 'text' 또는 'password' 중 하나를 받아옵니다.
+    value: string; // input 필드의 값 속성, 문자열 형태로 받아옵니다.
+    isErrorMessage?: boolean; // 에러 메시지 여부를 나타내는 속성, 선택 사항이며 불리언 형태입니다.
+    buttonTitle?: string; // 버튼 제목 속성, 선택 사항이며 문자열 형태입니다. ?는 선택 사항을 의미합니다.
+    message?: string; // 메시지 속성, 선택 사항이며 문자열 형태입니다.
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void; 
+    onKeydown?: (event: KeyboardEvent<HTMLInputElement>) => void; //?는 와도 되고 안와도 된다는 뜻이다, 엔터 누를시 동작
+    onButtonClick?: () => void; // 모든 버튼 클릭 시 호출되는 함수, 선택 사항입니다.
 }
 
 
@@ -14,18 +24,27 @@ interface Props{
 // <HTMLInputElement(=input 요소의 타입을 지정),Props(=Props는 사용자가 정의한 타입으로, 이 컴포넌트가 어떤 속성들을 받을지 정의)>
 const InputBox = forwardRef<HTMLInputElement,Props>((props: Props,ref)=>{
 
+    //인터페이스 Props에서 정의한 속성들을 컴포넌트의 props로 받았을 때, 각 속성을 알맞은 타입으로 지정해주는 의미
+    const { title, placeholder, type, value, isErrorMessage, buttonTitle,message, onChange, onKeydown, onButtonClick } = props;
+
+    //빈 문자열('')이면 'input-box-button-disable' 클래스를 사용 렇지 않으면 'input-box-button' 클래스를 사용 밑의도 같음
+    const buttonClass = value  === '' ? 'input-box-button-disable' : 'input-box-button';
+    const messageClass = isErrorMessage ? 'input-box-message-error': 'input-box-message';
+
+
     //반환할 html을 작성
+    //{}안에다가 매칭해주는 변수들은 다 외부에서 받아오는 값들을 대입한다 
     return (
         <div className='input-box'>
            
-            <div className='input-box-title'>{'아이디'}</div>
+            <div className='input-box-title'>{title}</div>
            
             <div className='input-box-content'>
                 <div className='input-box-body'>
-                    <input className='input-box-input' placeholder='아이디를 입력해주세요'/>
-                    <div className='input-box-button-disable'>{'중복 확인'}</div>
+                    <input ref={ref} className='input-box-input' placeholder={placeholder} type={type} value={value} onChange={onChange} onKeyDown={onKeydown}/>
+                    {buttonTitle !== undefined && onButtonClick !== undefined && <div className={buttonClass} onClick={onButtonClick}>{buttonTitle}</div>}
                 </div>
-                <div className='input-box-message-error'>{'사용 가능한 아이디 입니다.'}</div>
+                {message !== undefined && <div className={messageClass}>{message}</div>}
             </div>
         </div>
     );
